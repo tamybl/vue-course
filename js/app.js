70680d3030bf011815bb55866d1ad1e1812754d9
tamybl <tamara.bravo.l@gmail.com>
2019-02-19 13:41:10 -0300

    manejando eventos vue, metodos y computed

--- js/app.js
+++ js/app.js
@@ -3,30 +3,47 @@ const app = new Vue({
     data: {
         title: 'Hola mundo con Vue',
         fruits: [{
-            name: 'platano',
-            state: false,
-            quantity: 6
-        },
-        {
-            name: 'manzana',
-            state: true,
-            quantity: 0
-        }],
+                name: 'platano',
+                state: false,
+                quantity: 6
+            },
+            {
+                name: 'manzana',
+                state: true,
+                quantity: 0
+            }
+        ],
         newFruit: '',
-        newQuantity: ''
+        newQuantity: '',
+        total: 0
     },
     methods: {
         addFruit() {
             console.log('click');
             if (this.newFruit !== '' && this.newQuantity !== '') {
                 this.fruits.push({
-                    name: this.newFruit, quantity: this.newQuantity
+                    name: this.newFruit,
+                    quantity: this.newQuantity
                 })
                 this.newFruit = '', this.newQuantity = '';
 
             }
 
         }
+    },
+    computed: {
+        totalStock() {
+            this.total = 0;
+            this.total = this.fruits.reduce((a, b) => a.quantity + b.quantity);
+            return this.total;
+        }
+        // totalStock() {
+        //     this.total = 0;
+        //     for (fruit of this.fruits) {
+        //         this.total = this.total + fruit.quantity;
+        //     }
+        //     return this.total;
+        // }
     }
 
 })
