cd5e0d841e9f61012f245d2ebab493cbf6d22223
tamybl <tamara.bravo.l@gmail.com>
2019-02-21 13:42:31 -0300

    to do list con CRUD y localstorage

--- 05_todolist-app/js/script.js
+++ 05_todolist-app/js/script.js
@@ -5,14 +5,32 @@ const app = new Vue({
         taskList: [],
         newTask: ''
     },
+    created: function () {
+        let db = JSON.parse(localStorage.getItem('task-vue'));
+        if (db === null) {
+            this.taskList = [];
+        }
+        else {
+            this.taskList = db;
+        }
+    },
     methods: {
         addTask: function () {
             this.taskList.push({
                 text: this.newTask,
                 state: false
-            })
+            });
+            localStorage.setItem('task-vue', JSON.stringify(this.taskList));
             this.newTask = '';
             console.log(this.taskList);
+        },
+        changeState: function (index) {
+            this.taskList[index].state = true;
+            localStorage.setItem('task-vue', JSON.stringify(this.taskList));
+        },
+        deleteTask: function(index) {
+            this.taskList.splice(index, 1);
+            localStorage.setItem('task-vue', JSON.stringify(this.taskList));
         }
     }
 })
